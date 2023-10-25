0f172f11bb1ea146cd4df7ce69ec2f111cdbdae4
mahalingam-iyer <mahalingam.m.iyer@gmail.com>
2023-10-25 14:29:20 +0530

    Update App.jsx

--- react_app/src/App.jsx
+++ react_app/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [count, setCount] = useState('loading')
   useEffect(() => {
     async function fetchData(){
-      await fetch(import.meta.env.VITE_API_URL)
+      await fetch('http://13.232.231.224:3000/')
       .then((response) => response.json())
       .then((data) => setCount(data))
     }
