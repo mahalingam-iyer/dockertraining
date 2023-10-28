516c05db1ebab4a192744e28a78398fccc9c3799
mahalingam-iyer <mahalingam.m.iyer@gmail.com>
2023-10-28 20:22:00 +0530

    Update App.jsx
    removed async

--- react_app/src/App.jsx
+++ react_app/src/App.jsx
@@ -4,12 +4,9 @@ import './App.css'
 function App() {
   const [count, setCount] = useState('loading')
   useEffect(() => {
-    async function fetchData(){
-      await fetch('http://3.108.190.95:3000/')
+    fetch('http://3.108.190.95:3000/')
       .then((response) => response.json())
       .then((data) => setCount(data))
-    }
-    fetchData();
     console.log(import.meta.env.VITE_API_URL);
   }, []);
   return (
